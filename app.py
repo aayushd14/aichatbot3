8cbb4f89cba6a57312b3422398fee6df6608fd66
Raushan Kumar <rkds.py@gmail.com>
2026-01-28 12:40:23 +0530

    second commit

--- app.py
+++ app.py
@@ -199,5 +199,3 @@ def query():
 # --------------------------------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-print("Raushan Kumar")
-print("Learning Git")
